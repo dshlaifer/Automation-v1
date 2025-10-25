a01a5be0e2d80a6fb1ac5f42184a2128a6765ff1
david <dshlaifer@gmail.com>
2025-10-25 19:10:40 -0400

    questionjs 26 - 32

--- Automaton.java
+++ Automaton.java
@@ -46,29 +46,24 @@ public class Automaton
      */
     public void update()
     {
-        // Build the new state in a separate array.
-        int[] nextState = new int[state.length];
-        // Naively update the state of each cell
-        // based on the state of its two neighbors.
-        for(int i = 0; i < state.length; i++) {
-            int left, center, right;
-            if(i == 0) {
-                left = 0;
-            }
-            else {
-                left = state[i - 1];
-            }
-            center = state[i];
-            if(i + 1 < state.length) {
+        int left = 0;  //.. imaginary left neighbor of cell 0
+        int center = state[0];  // current cell
+        int right;
+        for (int i = 0; i < state.length; i++) {
+            //determine right neighbor (next cell)
+            if (i + 1 < state.length) {
                 right = state[i + 1];
-            }
-            else {
-                right = 0;
-            }
-            nextState[i] = (left + center + right) % 2;
-        }
-        state = nextState;
+            
+        } else { 
+            right = 0;
     }
+}
+int newValue = (left + center + right) % 2;
+left = center;
+center = right;
+// new value to current position
+state[i] = newValue;
+}
     
     /**
      * Reset the automaton.
@@ -77,6 +72,10 @@ public class Automaton
     {
         Arrays.fill(state, 0);
         // Seed the automaton with a single 'on' cell.
-        state[numberOfCells / 2] = 1;
+        // Randomly turn on 5 cells
+        for (int i = 0; i <5; i++) {
+            int index = (int)(Math.random() * numberOfCells);
+            state[index] = 1;
     }
+}
 }
